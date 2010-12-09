163d4f5bd775fda97c42d558be892c652787f447
Tony <ptonyweir@gmail.com>
2010-12-09 13:31:15 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lizzie04::Application.config.secret_token = '41bc916f81a01bb3a8fa31a3e1fa7994cce44c28370a8e8af132914c74b26622c50ca12b6f79b4afb0fdaf7200089596a74627983f13624a66ed476616da4bc1'
